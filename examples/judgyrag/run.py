b71803fb4eb91b8c8aef6c0d01a4c232076287f2
Eduardo Alvarez <57263404+eduand-alvarez@users.noreply.github.com>
2024-02-23 00:50:27 -0600

    Delete examples/judgyrag/run.py

--- examples/judgyrag/run.py
+++
@@ -1,3 +0,0 @@
-from fundialogues import judgypdf
-
-judgypdf(folder_path = r'C:\Users\Eduardo A Alvarez\Documents\fun_dialogues\SWE\judgyrag', output_directory = r'C:\Users\Eduardo A Alvarez\Documents\fun_dialogues\SWE\judgyrag\test2')
